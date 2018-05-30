4109a07e0f3d3974c21683cfb916620361db446e
공찬형 <forlennykong@gmail.com>
2018-05-31 00:42:23 +0900

    Final Version with Comment.

--- src/c0_computation/Dot_rule.java
+++ src/c0_computation/Dot_rule.java
@@ -1,11 +1,24 @@
+/**
+ * @Copyrighted Lenny_kong. (20133950 / °øÂùÇü)
+ * Email : dadasa13@naver.com
+ * Git_repository : github.com/syndra/Compiler_project_02
+ * 
+ * Please Check comment in each java file.
+ */
 package c0_computation;
 
+/*
+ * Basic object for production rule with dot.
+ */
 public class Dot_rule{
 
-	public char start;
-	public StringBuffer rule;
-	public int dot_index;
+	public char start;			// Start Non-terminal.
+	public StringBuffer rule;	// Righthand rule.
+	public int dot_index;		// Index of dot location.
 	
+	/*
+	 * Constructor.
+	 */
 	public Dot_rule(char start, String rule, int dot_index) {
 		this.start = start;
 		this.rule = new StringBuffer();
@@ -16,7 +29,11 @@ public class Dot_rule{
 		}
 		this.dot_index = dot_index;
 	}
-
+	
+	/*
+	 * Get character after dot.
+	 * E->.T => return T.
+	 */
 	public char get_char_next_dot() 
 	{
 		if(dot_index == rule.length())
@@ -24,12 +41,18 @@ public class Dot_rule{
 		return this.rule.charAt(this.dot_index);
 	}
 	
+	/*
+	 * If, dot can be moved, move it right.
+	 */
 	public void dot_move_right() 
 	{
 		if(dot_index != rule.length())
 			dot_index++;
 	}
 	
+	/*
+	 * Return rule String contains dot.
+	 */
 	public String get_dot_string() 
 	{
 		StringBuffer rule = new StringBuffer();
@@ -44,11 +67,17 @@ public class Dot_rule{
 		return rule.toString();
 	}
 	
+	/*
+	 * Print method to show process.
+	 */
 	public void print_dot_rule() 
 	{
 		System.out.println(this.start + ">" + this.get_dot_string());
 	}
 	
+	/*
+	 * Return true, if this Dot_rule is exactly same with operand.
+	 */
 	public boolean is_equal(Dot_rule operand) 
 	{
 		int strComp = operand.rule.toString().compareTo(operand.rule.toString()); 
--- src/c0_computation/Main.java
+++ src/c0_computation/Main.java
@@ -1,3 +1,10 @@
+/**
+ * @Copyrighted Lenny_kong. (20133950 / °øÂùÇü)
+ * Email : dadasa13@naver.com
+ * Git_repository : github.com/syndra/Compiler_project_02
+ * 
+ * Please Check comment in each java file.
+ */
 package c0_computation;
 
 import java.io.IOException;
@@ -6,7 +13,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException
 	{
-		C0_parser parser = new C0_parser("rule.txt");
-		parser.write_to_file();
+		C0_parser parser = new C0_parser("rule.txt");	// Make instance of parser with "rule.txt".
+		parser.write_to_file("output.txt");				// Save same directory with filename "output.txt".
 	}
 }
